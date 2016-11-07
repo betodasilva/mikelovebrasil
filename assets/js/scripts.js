eb7d127cdbc1c5d590a7a14aa97890fa56e2f13e
Beto <jrs_robert@hotmail.com>
2016-11-07 11:00:41 -0300

    add left menu animated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,30 @@
 window.onload = function(){
 	var menu = document.getElementById('nav-main'), 
 		openMenu = document.getElementById('open-menu'),
-		button = document.getElementById('btn'),
+		closeMenu = document.getElementById('close-menu'),
+		openBtn = document.getElementById('open-btn'),
+		closeBtn = document.getElementById('close-btn'),
 		body = document.body,
 		screenW = screen.width;
 
 	openMenu.onclick = function () {
 
-			openMenu.classList.toggle('active');
-			menu.classList.toggle('nav-open--left');
-			body.classList.toggle('nav--push-to-right');
-			button.classList.toggle('btn--closed');	
+			// openMenu.classList.toggle('active');
+			menu.classList.add('nav-open--left');
+			// body.classList.add('nav--push-to-right');
+			closeBtn.classList.remove('btn--closed');
+			openBtn.classList.add('btn--closed');
+			openBtn.classList.add('btn--push-to-right');
 	};
 
+	closeMenu.onclick = function () {
+
+			// openMenu.classList.toggle('active');
+			menu.classList.remove('nav-open--left');
+			openBtn.classList.remove('btn--push-to-right');
+			openBtn.classList.remove('btn--closed');
+			closeBtn.classList.add('btn--closed');
+	};
 	
 
 };
